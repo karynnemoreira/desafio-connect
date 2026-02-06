92d6039e0040913f988523458363842c81a3bb5f
karynnemoreira <karynnemoreira@yahoo.com.br>
2026-02-06 19:39:14 -0300

    Finalização da página inicial e conclusão da página de perfil

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import styles from "./Perfil.module.scss";
+import ImgPerfil from "../../assets/perfil.jpeg"
 
 function Perfil() {
   return (
-    <>
-      <h2>Perfil</h2>
-    </>
+    <section className={styles.containerTelaPerfil}>
+      <article>
+        <img src={ImgPerfil} alt="Foto de perfil" />
+
+        <div className={styles.cardInfo}>
+          <h2> Karynne Moreira </h2>
+          <h3> Voluntária Ativa</h3>
+
+          <p className={styles.paragrafoSobreMim}>
+            Apaixonada por fazer a diferença na comunidade. Acredito que
+            pequenas ações podem transformar vidas e estou sempre em busca de
+            novas oportunidades para ajudar.
+          </p>
+
+          <address>
+            <p> Rio de Janeiro, RJ</p>
+            <p>karynnemoreira@email.com</p>
+            <p> Membro desde Janeiro de 2022</p>
+          </address>
+
+          <ul className={styles.listaInteresses}>
+            <li>Educação</li>
+            <li>Meio Ambiente</li>
+            <li>Assistência Social</li>
+            <li>Design</li>
+          </ul>
+        </div>
+
+      </article>
+    </section>
   );
 }
 
 export default Perfil;
+
